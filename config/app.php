47780174ae6d71acbedfe016358a6cad907be41a
hazimhadis89 <hazim.hadis@gmail.com>
2019-12-25 13:06:37 +0800

    Revert "- app name: Laravel68"
    This reverts commit eda1b16eedc7f383f3df7e41ab550a0d2af756f2.

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel68'),
+    'name' => env('APP_NAME', 'Laravel'),
 
     /*
     |--------------------------------------------------------------------------
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://laravel68.local'),
+    'url' => env('APP_URL', 'http://localhost'),
 
     'asset_url' => env('ASSET_URL', null),
 
