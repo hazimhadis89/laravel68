bd9e1668454f3e535c9acd59dccf6ac42a89169e
hazimhadis89 <hazim.hadis@gmail.com>
2019-12-25 14:13:44 +0800

    update route (web & api)

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,6 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+Route::group(['middleware' => 'auth'], function() {
+    Route::get('/home', ['as'=>'home', 'uses'=>'HomeController@index']);
+});
